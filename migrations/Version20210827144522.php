181493cf05f2e460dad43ec652aa264cfbb43129
Tanguy CAMUS <tanguy.camus2021@campus-eni.fr>
2021-08-30 14:25:07 +0200

    TP10

---
+++ migrations/Version20210827144522.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210827144522 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(250) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE wish (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, title VARCHAR(250) NOT NULL, description VARCHAR(255) DEFAULT NULL, author VARCHAR(50) NOT NULL, is_published TINYINT(1) NOT NULL, date_created DATETIME NOT NULL, INDEX IDX_D7D174C912469DE2 (category_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE wish ADD CONSTRAINT FK_D7D174C912469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE wish DROP FOREIGN KEY FK_D7D174C912469DE2');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('DROP TABLE wish');
+    }
+}
--- src/Util/Censurator.php
+++ src/Util/Censurator.php
@@ -1,18 +1,23 @@
 <?php
+
 namespace App\Util;
 
-class Censurator{
+class Censurator
+{
 
     private $motInterdit = ['pute', 'enculé', 'cocaïne'];
 
-    public function purify(string $text){
-        foreach ($this->motInterdit as $mot){
-            $nbDigit = strlen($mot);
-            $censored = "";
-            for ($i = 0; $i < $nbDigit; $i++){
-                $censored .= "*";
+    public function purify(?string $text)
+    {
+        if (strlen($text)) {
+            foreach ($this->motInterdit as $mot) {
+                $nbDigit = strlen($mot);
+                $censored = "";
+                for ($i = 0; $i < $nbDigit; $i++) {
+                    $censored .= "*";
+                }
+                $text = str_ireplace($mot, "$censored", $text);
             }
-            $text = str_ireplace($mot, "$censored", $text);
         }
         return $text;
     }
